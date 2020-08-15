e251794814b297fe3c616473ab73fb6bfce6f93b
chebypax <chebypax@mail.ru>
2020-08-15 19:52:26 +0300

    Add stack and queue

--- main.js
+++ main.js
@@ -1,34 +1,19 @@
 import SinglyLinkedList from './structures/SinglyLinkedList';
 import DoublyLinkedList from "./structures/DoublyLinkedList";
+import Stack from "./structures/Stack";
+import Queue from "./structures/Queue";
 
-const list = new DoublyLinkedList();
+const stack = new Queue();
 
-list.push('PRIVET');
-list.push(' MY ');
-list.push('NIGGER');
-list.push('!!!');
-list.traverse();
-console.log(list);
-// console.log(list.remove(2));
+console.log(stack.enqueue('first'));;
+console.log(stack.enqueue('second'));;
+console.log(stack.enqueue('third'));;
 
-// console.log(list.unshift('PRIVET1'));;
-// console.log(list.unshift(' MY1 '));;
-// console.log(list.unshift('NIGGER1'));;
-list.traverse();
-list.reverse();
+console.log(stack);
 
-// console.log(list.set(-1, 'TEST'));
-// console.log(list.set(0, 'TEST0'));
-// console.log(list.set(2, 'TEST2'));
-// console.log(list.insert(15, 'TEST'));
+console.log(stack.dequeue());
+console.log(stack.dequeue());
+console.log(stack.dequeue());
+console.log(stack.dequeue());
 
-
-
-// console.log(list.shift());
-// list.traverse();
-// console.log(list.shift());
-// list.traverse();
-// console.log(list.shift());
-list.traverse();
-// console.log(list.shift());
-console.log(list);
+console.log(stack);
---
+++ structures/Stack/index.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) {
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+    }
+    this.first = newNode;
+    this.size += 1;
+
+    return this.size;
+  }
+
+  pop() {
+    if (this.size === 0) {
+      return undefined;
+    }
+    const deletedNode = this.first;
+    this.first = deletedNode.next;
+    deletedNode.next = null;
+    this.size -= 1;
+
+    if (this.size === 0) {
+      this.last = null;
+    }
+
+    return deletedNode;
+  }
+}
+
+export default Stack;
